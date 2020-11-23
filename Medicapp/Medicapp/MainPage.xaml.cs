c38625fbde99a8c202f9f2f4c28dff3d31ff4cc6
Sergio Andres Osorio Lozano <sergyo0212@gmail.com>
2020-11-22 19:08:10 -0500

    actualizacion

---
+++ Medicapp/Medicapp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Medicapp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnregistro_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new registro());
+        }
+
+        private async void btnlogin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new login());
+        }
+    }
+}
